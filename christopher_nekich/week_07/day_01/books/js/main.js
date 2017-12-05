24c4d4385ececbcad26f506abd30ab3b609901a9
chriswillphoto <christophernekich@gmail.com>
2017-12-05 11:08:02 +1100

    books

--- christopher_nekich/week_07/day_01/books/js/main.js
+++ christopher_nekich/week_07/day_01/books/js/main.js
@@ -1,5 +1,3 @@
-"https://www.googleapis.com/books/v1/volumes?q="
-
 
 let searchBar = document.querySelector("input");
 let title = document.querySelector(".booktitle")
@@ -12,12 +10,13 @@ const fetchBook = function(){
   const xhr = new XMLHttpRequest()
 
   xhr.onreadystatechange = function() {
-  if(xhr.readyState !== 4) return;
-  const info = JSON.parse( xhr.response )
-  title.innerHTML = info.items[0].volumeInfo.title;
-  cover.setAttribute("src", info.items[0].volumeInfo.imageLinks.thumbnail )
-  description.innerHTML = info.items[0].volumeInfo.description
-  author.innerHTML = "by " + info.items[0].volumeInfo.authors.join(" & ")
+    if(xhr.readyState !== 4) return;
+    const info = JSON.parse( xhr.response )
+    title.innerHTML = info.items[0].volumeInfo.title;
+    cover.setAttribute("src", info.items[0].volumeInfo.imageLinks.thumbnail )
+    description.classList.remove("hidden")
+    description.innerHTML = info.items[0].volumeInfo.description
+    author.innerHTML = "by " + info.items[0].volumeInfo.authors.join(" & ")
 
   }
   xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=${ searchBar.value }`);

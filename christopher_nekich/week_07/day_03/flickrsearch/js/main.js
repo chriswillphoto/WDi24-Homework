3ee30ca1c7e8919e108690582ab3b5f5e9ab2851
chriswillphoto <christophernekich@gmail.com>
2017-12-07 09:58:31 +1100

    minor changes

--- christopher_nekich/week_07/day_03/flickrsearch/js/main.js
+++ christopher_nekich/week_07/day_03/flickrsearch/js/main.js
@@ -4,13 +4,16 @@ let _pagelimit = 1
 
 const searchFlickr = function( term, page ) {
   const flickrURL = 'https://api.flickr.com/services/rest/?jsoncallback=?';
-  $.getJSON(flickrURL, {
-    method: 'flickr.photos.search',
-    api_key: '2f5ac274ecfac5a455f38745704ad084',
-    text: term,
-    format: 'json',
-    page: page
-  }).done(showImages)
+
+  if(term){
+    $.getJSON(flickrURL, {
+      method: 'flickr.photos.search',
+      api_key: '2f5ac274ecfac5a455f38745704ad084',
+      text: term,
+      format: 'json',
+      page: page
+    }).done(showImages)
+  }
 }
 
 const showImages = function(results) {
@@ -55,20 +58,26 @@ $(document).ready( function() {
 
   $(window).on("scroll", _.debounce(function() {
     const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop()
-
+    console.log("yes")
     if( pagenum <= _pagelimit ){
-      if(scrollBottom > 600) {
+      if(scrollBottom > 900) {
         return;
       }
         const query = $("#query").val();
         searchFlickr(query, pagenum)
         pagenum += 1
     }
-  }, 1800)
+  }, 800)
   )
 
   $("#images").on("click", "img", function() {
     bigurl = $(this).attr("src").slice(0, -5) + "b.jpg"
+    // id = $(this).attr("src").slice(37, 47)
+    // $.ajax('https://api.flickr.com/services/rest/?jsoncallback=?', {
+    //   method: "flickr.photos.getInfo",
+    //   api_key: "2f5ac274ecfac5a455f38745704ad084",
+    //   photo_id: id
+    // }).done(function(results){console.log(results)})  // was going to add a link to view on flickr
     $("<img/>").appendTo("#viewer").attr("src", bigurl)
     $("#viewer").fadeIn(700)
 
